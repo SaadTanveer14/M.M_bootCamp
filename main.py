3a00af7bba4106eca0991581e48a8b6f49925faf
SaadTanveer14 <99664282+SaadTanveer14@users.noreply.github.com>
2023-07-19 00:38:04 +0500

    BootCamp Day2 Assignment

--- main.py
+++ main.py
@@ -1,1 +1,26 @@
-print('Hello World')
+
+# first name variable
+firstName = "Muhammad Saad"
+
+# last name variable
+lastName = "Tanveer"
+
+# age variable
+age = 23
+
+# concatinating string variables
+fullName = firstName + " " + lastName
+
+print("Full Name :- " + fullName)
+
+print("Age :- " + str(age))
+
+
+# type of first name variable
+print(type(firstName))
+
+# type of last name variable
+print(type(lastName))
+
+# type of age variable
+print(type(age))
