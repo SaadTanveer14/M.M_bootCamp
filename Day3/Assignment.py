c4fc4fe2e7b30d2019f5f80e26f3bf6c5649df0b
SaadTanveer14 <99664282+SaadTanveer14@users.noreply.github.com>
2023-07-19 22:32:10 +0500

    BootCamp Day 3

---
+++ Day3/Assignment.py
@@ -0,0 +1,37 @@
+# Question#01
+# Write a python program in which initialize 2 integer variables 
+# add 2 numbers
+# subtract 2 numbers
+# multiple 2 numbers
+# divide 2 numbers
+
+number_1 = 5
+
+number_2 = 2
+
+#adding numbers
+addNumber = number_1 + number_2
+
+#subtracting numbers
+subtractNumber = number_1 - number_2
+
+#multiplying numbers
+multiplyNumber = number_1 * number_2
+
+#dividing numbers
+divideNumber = number_1 / number_2
+
+
+print(addNumber, subtractNumber, multiplyNumber, divideNumber)
+
+
+
+#Question#02
+# Write a python program in which initialize 2 integer variables 
+# check number 1  is greater then number 2.
+
+# number already initialized
+
+result = number_1 > number_2
+
+print(result)
